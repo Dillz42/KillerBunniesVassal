fd9db97d5aa4be5ecf701075a06f96cda78fb473
Dillz42 <Dillz42@gmail.com>
2015-11-02 18:55:51 -0600

    Initial Commit
    Commit development files

---
+++ Killer Bunnies/Booster Websites/Orange Booster Deck (Quest) - Killer Bunnies Wiki_files/market.js
@@ -0,0 +1,17 @@
+var rp_response = 
+    { "valuation": { "pmp": { "seat_ids": [ 2443, 2678, 3118, 4007, 4958, 5650, 7950, 9462, 10284 ], "deals": [ { "id": 24698, "class": 2, "floor": 2.500000, "ad_ids": [ 3162067 ], "network_ids": [ 1185 ] }, { "id": 46934, "class": 3, "floor": 4.000000, "ad_ids": [ 3212456 ], "network_ids": [ 2249 ] }, { "id": 48692, "class": 3, "floor": 2.200000, "ad_ids": [ 3373228 ], "network_ids": [ 2760 ] }, { "id": 48694, "class": 3, "floor": 2.500000, "ad_ids": [ 3373228 ], "network_ids": [ 2760 ] }, { "id": 49916, "class": 3, "floor": 3.000000, "ad_ids": [ 3236011 ], "network_ids": [ 2307 ] }, { "id": 64486, "class": 2, "floor": 2.750000, "ad_ids": [ ], "network_ids": [ 1185, 1512, 1523, 1902, 1986, 1994, 2046, 2081, 2082, 2179, 2181, 2238, 2243, 2245, 2249, 2271, 2307, 2313, 2365, 2395, 2528, 2596, 2676, 2682, 2760, 2810, 2915, 2974, 2978, 3105, 3272, 3320, 3404, 3498 ] }, { "id": 64488, "class": 2, "floor": 2.750000, "ad_ids": [ ], "network_ids": [ 1185, 1512, 1523, 1902, 1986, 1994, 2046, 2081, 2082, 2179, 2181, 2238, 2243, 2245, 2249, 2271, 2307, 2313, 2365, 2395, 2528, 2596, 2676, 2682, 2760, 2810, 2915, 2974, 2978, 3105, 3272, 3320, 3404, 3498 ] }, { "id": 68544, "class": 3, "floor": 5.000000, "ad_ids": [ 3450174 ], "network_ids": [ 2974 ] }, { "id": 69084, "class": 3, "floor": 3.000000, "ad_ids": [ 3236011 ], "network_ids": [ 2307 ] }, { "id": 72536, "class": 2, "floor": 13.000000, "ad_ids": [ 3161859 ], "network_ids": [ 1512 ] }, { "id": 72548, "class": 2, "floor": 3.500000, "ad_ids": [ 3161859 ], "network_ids": [ 1512 ] }, { "id": 72924, "class": 2, "floor": 5.000000, "ad_ids": [ 3168960 ], "network_ids": [ 1986 ] }, { "id": 72926, "class": 2, "floor": 3.000000, "ad_ids": [ 3168960 ], "network_ids": [ 1986 ] }, { "id": 78758, "class": 2, "floor": 3.000000, "ad_ids": [ 3216516, 3426417 ], "network_ids": [ 2271 ] }, { "id": 78760, "class": 2, "floor": 5.000000, "ad_ids": [ 3216516, 3426417 ], "network_ids": [ 2271 ] }, { "id": 78762, "class": 2, "floor": 3.000000, "ad_ids": [ 3216516, 3426417 ], "network_ids": [ 2271 ] }, { "id": 82188, "class": 2, "floor": 3.000000, "ad_ids": [ ], "network_ids": [ 1185, 1512, 1523, 1902, 1986, 1994, 2046, 2081, 2082, 2179, 2181, 2238, 2243, 2245, 2249, 2271, 2307, 2313, 2365, 2395, 2528, 2596, 2676, 2682, 2760, 2810, 2915, 2974, 2978, 3105, 3272, 3320, 3404, 3498 ] }, { "id": 82960, "class": 2, "floor": 2.930000, "ad_ids": [ 3236011 ], "network_ids": [ 2307 ] }, { "id": 85984, "class": 3, "floor": 4.000000, "ad_ids": [ 3236011 ], "network_ids": [ 2307 ] }, { "id": 86026, "class": 3, "floor": 3.500000, "ad_ids": [ 3176291, 3455364 ], "network_ids": [ 2081 ] }, { "id": 87372, "class": 3, "floor": 5.000000, "ad_ids": [ 3450174 ], "network_ids": [ 2974 ] } ] }, "ads": [ { "type": "house", "cpm": 0.000000, "dcpm": 0.000000, "ad_id": 3689104 } ], "cpm_cnt": 1, "invalid_cpm_cnt": 0, "bid_cnt": 0, "invalid_bid_cnt": 0 }, "context": { "site_session_count": "0", "country": "us" } };
+
+var rp_valuation = rp_response.valuation;
+try { oz_onValuationLoaded_175094_2(rp_response); } catch (ignore) {}
+
+/*
+Data Center: IAD1
+pid: 4760
+BE Status: O
+BEms: 5
+AEms: 9
+Alg: PT
+Session Count: 1
+Continent: na
+Country: us
+*/
